7465ff6ae996801e58fa62c6be1c47971bfe48e4
Pro <aadishresth@gmail.com>
2026-05-12 16:33:00 +0530

    Allows input() as a list

---
+++ linear_expressions.py
@@ -0,0 +1,8 @@
+def linear_expressions(vector_1, vector_2):
+    if len(vector_1) != len(vector_2):
+        return "Error"
+
+v1 = [float(x) for x in input("Enter values: ").split()]
+v2 = [float(x) for x in input("Enter values: ").split()]
+
+linear_expressions(v1, v2)
