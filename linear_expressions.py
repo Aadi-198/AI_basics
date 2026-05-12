c00c123a4a557802fa38ca73d5663f4fee269714
Pro <aadishresth@gmail.com>
2026-05-12 19:57:07 +0530

    Made it working

--- linear_expressions.py
+++ linear_expressions.py
@@ -1,8 +1,12 @@
 def linear_expressions(vector_1, vector_2):
     if len(vector_1) != len(vector_2):
         return "Error"
+    
+    result = sum(a * b for a, b in zip(vector_1, vector_2))
+    return result
 
 v1 = [float(x) for x in input("Enter values: ").split()]
 v2 = [float(x) for x in input("Enter values: ").split()]
 
-linear_expressions(v1, v2)
+output = linear_expressions(v1, v2)
+print(f"The output is {output:g}")
